f2cb06f6766baeac4602190e55605df0acb7411c
Ciera Watts <cwatts@catalyte.io>
2026-07-13 10:51:13 -0500

    Fix Purchase model: update items attribute to use correct list type annotation

--- models/purchase.py
+++ models/purchase.py
@@ -1,12 +1,15 @@
 from dataclasses import dataclass
 import datetime
-from models.customer import Customer
+from models.customer import Customer, Ingredient
+from models.drink import Drink
+from models.baked_good import Ingredient
+
 
 
 @dataclass
 class Purchase:
     timestamp: datetime
-    items: list[]
+    items: list[Drink, Ingredient]
     total_cost: float
     Customer: Customer
     
