b9d422b3f0a3aed60110d095211d2eb5b093741b
Ciera Watts <cwatts@catalyte.io>
2026-07-13 11:09:39 -0500

    Fix Baked_good class name and update Purchase model to use Baked_good type

--- models/purchase.py
+++ models/purchase.py
@@ -2,14 +2,14 @@ from dataclasses import dataclass
 import datetime
 from models.customer import Customer, Ingredient
 from models.drink import Drink
-from models.baked_good import Ingredient
+from models.baked_good import Baked_good
 
 
 
 @dataclass
 class Purchase:
     timestamp: datetime
-    items: list[Drink, Ingredient]
+    items: list[Drink, Baked_good]
     total_cost: float
     Customer: Customer
      
