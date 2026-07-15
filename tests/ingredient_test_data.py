aaa1fcb3b2f1658b616b027c4a0c1b27a419138b
Brennan Idor <bidor@catalyte.io>
2026-07-15 11:02:08 -0500

    Created test data for Drink and Ingredient

---
+++ tests/ingredient_test_data.py
@@ -0,0 +1,136 @@
+from models.ingredients import Ingredient
+from repositories.ingredients_repository import IngredientRepository
+
+i_00 = (
+    4000,
+    "Espresso Beans",
+    18.00,
+    2,
+    "lb"
+)
+i_01 = (
+    4001,
+    "Water",
+    1.20,
+    1,
+    "gallon"
+)
+i_02 = (
+    4002,
+    "Whole Milk",
+    4.10,
+    1,
+    "gallon"
+)
+i_03 = (
+    4003,
+    "Vanilla Syrup",
+    9.50,
+    750,
+    "ml"
+)
+i_04 = (
+    4004,
+    "Caramel Drizzle",
+    8.75,
+    500,
+    "ml"
+)
+i_05 = (
+    4005,
+    "Coarse Ground Coffee",
+    16.50,
+    2,
+    "lb"
+)
+i_06 = (
+    4006,
+    "Matcha Powder",
+    22.00,
+    1,
+    "lb"
+)
+i_07 = (
+    4007,
+    "Whole Milk",
+    4.10,
+    1,
+    "gallon"
+)
+i_08 = (
+    4008,
+    "Honey",
+    7.25,
+    24,
+    "oz"
+)
+i_09 = (
+    4009,
+    "Chocolate Syrup",
+    6.90,
+    750,
+    "ml"
+)
+i_10 = (
+    4010,
+    "Ice",
+    2.00,
+    10,
+    "lb"
+)
+i_11 = (
+    4011,
+    "Whipped Cream",
+    5.40,
+    32,
+    "oz"
+)
+i_12 = (
+    4012,
+    "Chai Concentrate",
+    11.00,
+    32,
+    "oz"
+)
+i_13 = (
+    4013,
+    "Cinnamon",
+    4.50,
+    12,
+    "oz"
+)
+i_14 = (
+    4014,
+    "Cocoa Powder",
+    6.75,
+    16,
+    "oz"
+)
+i_15 = (
+    4015,
+    "Sugar",
+    3.20,
+    4,
+    "lb"
+)
+i_16 = (
+    4016,
+    "Oat Milk",
+    5.60,
+    64,
+    "fl oz"
+)
+i_17 = (
+    4017,
+    "Lavendar Syrup",
+    10.25,
+    750,
+    "ml"
+)
+
+def ingredient_dataset():
+    ingredients = IngredientRepository()
+    ingredient_instances = [globals()[f"i_{index:02d}"] for index in range (17)]
+    for ingredient in ingredient_instances:
+        ingredients.add(ingredient)
+    return ingredient
